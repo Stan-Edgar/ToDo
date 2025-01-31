793e5ad6ca96a37a2ba481575d7b4f0bd27e540a
Stan <stanedgar@gmail.com>
2025-01-30 20:32:54 -0600

    Made an edit class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 class Task {
 
-constructor(title, desc, date, priority, notes, checklist) {
+constructor(title, desc, date, priority, notes, done) {
 
 this.title = title;
 this.desc = desc;
 this.date = date;
 this.priority = priority;
 this.notes = notes;
-this.checklist = checklist;
+this.done = done;
 
+}
+
+
+}
+
+
+class Edit {
+
+constructor(task) {
+    this.task = task;
+}
+
+editTitle(title) {
+    this.task.title = title;
 }
 
 
@@ -16,3 +30,9 @@ this.checklist = checklist;
 
 const Workout = new Task("Evening Run", "doing HIIT", "1/30/2025", "high", "Need to get faster & build stamina", false);
 console.log(Workout);
+
+const eWorkout = new Edit(Workout);
+eWorkout.editTitle("Morning Run");
+console.log(eWorkout);
+
+
