3998f542b16d45e49904be982a4673e4b3a7d67c
Stan <stanedgar@gmail.com>
2025-01-31 21:32:17 -0600

    Made logic.js to import into index.js & started usng date-fns

--- src/index.js
+++ src/index.js
@@ -1,69 +1,8 @@
-class Task {
+import { compareAsc, format } from "date-fns";
+import {Task, Edit, Project} from "./logic.js";
 
-constructor(title, desc, date, priority, notes, done) {
-
-this.title = title;
-this.description = desc;
-this.date = date;
-this.priority = priority;
-this.notes = notes;
-this.done = done;
-
-}
+// new Date(year, monthIndex, day
 
 
-}
-
-
-class Edit {
-
-constructor(task) {
-    this.task = task;
-}
-
-editTitle(title) {
-    this.task.title = title;
-}
-
-editDesc(desc) {
-    this.task.description = desc;
-}
-
-editDate(date) {
-    this.task.date = date;
-}
-
-editPriority(priority) {
-    this.task.priority = priority;
-}
-editNotes(notes) {
-    this.task.notes = notes;
-}
-
-
-}
-
-class Project {
-
-    constructor(name, ...task) {
-        this.name = name;
-        this.task = task;
-    }
-
-}
-
-
-
-const Workout = new Task("Evening Run", "doing HIIT", "1/30/2025", "high", "Need to get faster & build stamina", false);
-console.log(Workout);
-
-const Meditation = new Task("Morning Meditation", "NSDR", "1/31/2025", "medium", "", false);
-
-const eWorkout = new Edit(Workout);
-eWorkout.editTitle("Morning Run"); // The edited version will replace the root version in the DOM
-eWorkout.editTitle("Afternoon Run"); // The edit version can be edited as many times as necessary.
-
-const Fun = new Project("Fun", Workout, eWorkout, Meditation);
-console.log(Fun);
-
-
+const work = new Task("Programming","Working on To-Do App", format(new Date(2025, 0, 31), "MM/dd/yyyy"), "high", "brainstorming app logic", false);
+console.log(work);
---
+++ src/logic.js
@@ -0,0 +1,69 @@
+export class Task {
+
+    constructor(title, desc, date, priority, notes, done) {
+    
+    this.title = title;
+    this.description = desc;
+    this.date = date;
+    this.priority = priority;
+    this.notes = notes;
+    this.done = done;
+    
+    }
+    
+    
+    }
+    
+    
+export class Edit {
+
+constructor(task) {
+    this.task = task;
+}
+
+editTitle(title) {
+    this.task.title = title;
+}
+
+editDesc(desc) {
+    this.task.description = desc;
+}
+
+editDate(date) {
+    this.task.date = date;
+}
+
+editPriority(priority) {
+    this.task.priority = priority;
+}
+editNotes(notes) {
+    this.task.notes = notes;
+}
+    
+    
+    }
+    
+export class Project {
+
+    constructor(name, ...task) {
+        this.name = name;
+        this.task = task;
+    }
+
+}
+    
+    
+
+export const Workout = new Task("Evening Run", "doing HIIT", "1/30/2025", "high", "Need to get faster & build stamina", false);
+
+export const Meditation = new Task("Morning Meditation", "NSDR", "1/31/2025", "medium", "", false);
+
+export const eWorkout = new Edit(Workout);
+eWorkout.editTitle("Morning Run"); // The edited version will replace the root version in the DOM
+eWorkout.editTitle("Afternoon Run"); // The edit version can be edited as many times as necessary.
+
+export const Default = new Project("Default", Workout, eWorkout, Meditation);
+
+    
+    
+    
